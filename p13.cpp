3c78f5cc1b477a1cad4f65e1a3556e8a8878f1c5
Manav Sharma Paudel <manavpaudel@Manavs-MacBook-Air.local>
2026-07-11 19:29:06 +0545

    Added p13.cpp

---
+++ p13.cpp
@@ -0,0 +1,38 @@
+// Implement bubble sort algorithm using function templates.
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void bubbleSort(T arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                T temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
